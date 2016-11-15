2dd556e3b3cc586093bfba06289b96ba6ae57f53
pengshuang <pengshuang92@163.com>
2016-11-15 19:05:58 +0800

    更新为 HBase 1.2

--- util/src/Counter/Count2.java
+++ util/src/Counter/Count2.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Increment;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.hsqldb.Table;
 
 import java.io.IOException;
 
@@ -16,7 +17,6 @@ public class Count2 {
     {
         Configuration conf = HBaseConfiguration.create();
         HTable hTable = new HTable(conf, "temp2");
-
         Increment increment1 = new Increment(Bytes.toBytes("20160101"));
         increment1.addColumn(Bytes.toBytes("daily"), Bytes.toBytes("clicks"), 1);
         increment1.addColumn(Bytes.toBytes("daily"), Bytes.toBytes("hits"), 1);
